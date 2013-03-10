1e02897b6d00fe6a8b35f16af900b079b901bffd
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-03-10 01:13:16 +0000

    Restructure LGE msm7x27a repos, move common config to shared path

--- e610.mk
+++ e610.mk
@@ -1,99 +1,32 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-
-# The gps config appropriate for this device
-$(call inherit-product, device/common/gps/gps_us_supl.mk)
+$(call inherit-product, device/lge/msm7x27a-common/msm7x27a-common.mk)
 
 $(call inherit-product-if-exists, vendor/lge/e610/e610-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS += device/lge/e610/overlay
 
-PRODUCT_TAGS += dalvik.gc.type-precise
 $(call inherit-product, $(LOCAL_PATH)/mdpi-dalvik.mk)
 
 PRODUCT_AAPT_CONFIG := normal mdpi
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/recovery/postrecoveryboot.sh:recovery/root/sbin/postrecoveryboot.sh
-
-PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/init.m4.rc:root/init.m4.rc \
-    $(LOCAL_PATH)/ueventd.m4.rc:root/ueventd.m4.rc \
-    $(LOCAL_PATH)/init.m4.usb.rc:root/init.m4.usb.rc
+    $(LOCAL_PATH)/ueventd.m4.rc:root/ueventd.m4.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
     $(LOCAL_PATH)/configs/m4_keypad.kl:/usr/keylayout/m4_keypad.kl \
     $(LOCAL_PATH)/configs/touch_mcs8000.idc:system/usr/idc/touch_mcs8000.idc \
-    $(LOCAL_PATH)/configs/touch_mcs8000.kl:system/usr/keylayout/touch_mcs8000.kl \
-    $(LOCAL_PATH)/configs/AudioFilter.csv:system/etc/AudioFilter.csv \
-    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
-
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml \
-    $(LOCAL_PATH)/configs/media_codecs.xml:system/etc/media_codecs.xml \
-    $(LOCAL_PATH)/configs/audio_policy.conf:system/etc/audio_policy.conf
-
-$(call inherit-product, build/target/product/full.mk)
-
-# Permission files
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
-    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
-    frameworks/native/data/etc/android.hardware.telephony.gsm.xml:system/etc/permissions/android.hardware.telephony.gsm.xml \
-    frameworks/native/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
-    frameworks/native/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
-    frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml \
-    frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.touchscreen.multitouch.jazzhand.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.jazzhand.xml
-
-PRODUCT_PACKAGES += \
-    lgapversion
+    $(LOCAL_PATH)/configs/touch_mcs8000.kl:system/usr/keylayout/touch_mcs8000.kl
 
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/wpa_supplicant.conf:system/etc/wifi/wpa_supplicant.conf \
     $(LOCAL_PATH)/prebuilt/librasdioif.ko:system/lib/modules/librasdioif.ko \
     $(LOCAL_PATH)/prebuilt/wlan.ko:system/lib/modules/wlan.ko
 
-# BT
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/init.qcom.bt.sh:system/etc/init.qcom.bt.sh
-
 # HW HALS
 PRODUCT_PACKAGES += \
-    libgenlock \
-    liboverlay \
-    gralloc.msm7x27a \
-    hwcomposer.msm7x27a \
-    copybit.msm7x27a \
     gps.e610 \
-    power.e610 \
-    audio.a2dp.default \
-    audio.primary.msm7x27a \
-    audio_policy.msm7x27a \
-    charger \
-    charger_res_images
+    power.e610
 
-# OMX 
-PRODUCT_PACKAGES += \
-    libstagefrighthw \
-    libmm-omxcore \
-    libOmxCore
-
-# NFC packages
-PRODUCT_PACKAGES += \
-    libnfc \
-    libnfc_jni \
-    Nfc \
-    Tag
-
-PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 PRODUCT_NAME := full_e610
 PRODUCT_DEVICE := e610
 PRODUCT_MODEL := LG-E610
-PRODUCT_MANUFACTURER := LGE
-PRODUCT_BRAND := lge
--- recovery/postrecoveryboot.sh
+++
@@ -1,3 +0,0 @@
-#!/sbin/sh
-
-echo 120 > /sys/devices/platform/i2c-gpio.5/i2c-5/5-0038/backlight/lm3530-bl/brightness
