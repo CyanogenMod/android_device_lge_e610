bb0b1b581fd8620f7cf9f42f9900f603272ab472
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-04-18 01:17:19 +0100

    Updated configurations for jb_2.5-based source

--- e610.mk
+++ e610.mk
@@ -13,19 +13,20 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/ueventd.m4.rc:root/ueventd.m4.rc
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/m4_keypad.kl:/usr/keylayout/m4_keypad.kl \
+    $(LOCAL_PATH)/configs/m4_keypad.kl:system/usr/keylayout/m4_keypad.kl \
     $(LOCAL_PATH)/configs/touch_mcs8000.idc:system/usr/idc/touch_mcs8000.idc \
     $(LOCAL_PATH)/configs/touch_mcs8000.kl:system/usr/keylayout/touch_mcs8000.kl
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+
 # Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/librasdioif.ko:system/lib/modules/librasdioif.ko \
     $(LOCAL_PATH)/prebuilt/wlan.ko:system/lib/modules/wlan.ko
 
 # HW HALS
 PRODUCT_PACKAGES += \
-    gps.e610 \
-    power.e610
+    gps.e610
 
 PRODUCT_NAME := full_e610
 PRODUCT_DEVICE := e610
